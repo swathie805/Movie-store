555cc1cc929cd2d7f83a3bce30227f71a5b8bd22
unknown <334587458@TDSB.ON.CA>
2023-06-02 13:49:42 -0400

    Made changes to hero of homepage

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,11 +1,16 @@
 <script setup>
-
 </script>
 
 <template>
-
+    <div class="hero-container">
+        <h2>Get with the trending movies and add them to your cart</h2>
+        <p>This is a movie website that displays trending movies along with content related to the film and a cart to purchase the movie. Enjoy </p>
+    </div>
 </template>
 
 <style scoped>
-
+hero-container {
+    background-color:  rgb(1, 1, 48);
+    background-image: url("https://www.thepcdoctor.com.au/wp-content/uploads/2020/12/Sci-Fi-Movies.jpg");
+}
 </style>
